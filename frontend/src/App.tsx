0cfc7ec8bfc5fad0e06bd1ff64d47779a8a5817c
rishabh7923 <rishabhyadav7923@gmail.com>
2026-03-05 11:08:30 +0530

    frontend setup is completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <p>This is the frontend!</p>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
